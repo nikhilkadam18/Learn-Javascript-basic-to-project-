1ca3e0355f1a6e18fbcbd3e1702e0547ff8925e5
Nikhil Kadam <83809232nk@gmail.com>
2023-03-17 19:20:59 +0530

    First commit

---
+++ LetConstVar01.js
@@ -0,0 +1,14 @@
+//let
+/*let greeting = "say Hi";
+    if (true) {
+        let greeting = "say Hello instead";
+        console.log(greeting); // "say Hello instead"
+    }
+    console.log(greeting);*/
+
+//const
+if (true) {
+    const greeting = "say Hello instead";
+    console.log(greeting); // "say Hello instead"
+}
+// console.log(greeting);
