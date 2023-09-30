443de4763f0d73cfb6baae2e018071f64d364c86
Nikhil Kadam <83809232nk@gmail.com>
2023-09-30 13:13:00 +0530

    add variables & datatypes practice files

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,18 @@
+const accountID = 12345
+let accountEmail = "NikhilGoogle@gmail.com"
+var accountPassword = "77777"
+accountCity = "mumbai"
+let accountState;
+
+//accountID = 22 //not allowed
+
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
