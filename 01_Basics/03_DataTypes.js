940c1710547847215a142be6e1308631701b88d9
Nikhil Kadam <83809232nk@gmail.com>
2023-10-01 12:56:32 +0530

    Data types

--- 01_Basics/03_DataTypes.js
+++ 01_Basics/03_DataTypes.js
@@ -1,1 +1,26 @@
-"use strict";
+"use strict";  //treat all JS code as newer version
+
+console.log(3+3);  
+
+let name = "nikhil";
+let age = 23
+let loggedIN = true
+
+console.log(name);
+console.log(age);
+console.log(loggedIN);
+
+
+//number
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
